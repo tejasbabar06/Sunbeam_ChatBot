ac05b3880a3ea10a6df3c1095ae5fc8dad4e8a97
tejasbabar06 <tejasbabar335@gmail.com>
2026-02-07 01:41:58 +0530

    Last one

--- chatbot.py
+++ chatbot.py
@@ -5,8 +5,10 @@ import tempfile
 # -----------------------------
 # Fix Python path for Streamlit Cloud
 # -----------------------------
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(ROOT_DIR)
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
 
 from dotenv import load_dotenv
 load_dotenv()
